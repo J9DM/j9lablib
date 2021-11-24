55c9cce55a34bc3b813372b814ef15c67596b15e
Jennine Dawicki-McKenna <jdmckenna00@gmail.com>
2021-11-24 18:35:45 -0500

    Fixed note.

--- R/plot_qpcr_target.R
+++ R/plot_qpcr_target.R
@@ -5,7 +5,9 @@
 #' @param color_by variable for coloring, quoted
 #' @param pch_by variable for plotting character, quoted
 #'
-#' @return A plot
+#' @importFrom rlang .data
+#'
+#' @return A plot of 2^(CT_ctrl - CT) for each target and condition
 #' @export
 #'
 # @examples
@@ -18,10 +20,14 @@ plot_qpcr_target <- function(df, facet_vars = c("target", "condition"),
   ctrl_condition <- df$ctrl %>% unique()
 
   df %>%
-    ggplot2::ggplot(aes(x = .data$replicate, y = .data$fold_ctrl)) +
-    ggplot2::facet_grid(vars(!! facet_vars[[1]]), vars(!! facet_vars[[2]])) +
+    ggplot2::ggplot(
+      ggplot2::aes(x = .data$replicate, y = .data$fold_ctrl)) +
+    ggplot2::facet_grid(
+      ggplot2::vars(!! facet_vars[[1]]),
+      ggplot2::vars(!! facet_vars[[2]])) +
     ggplot2::geom_hline(yintercept = 1, color = "red") +
     ggplot2::geom_boxplot() +
-    ggplot2::geom_point(aes(color = !! color_by, pch = !! pch_by)) +
+    ggplot2::geom_point(
+      ggplot2::aes(color = !! color_by, pch = !! pch_by)) +
     ggplot2::ylab(paste0("Fold change over ", ctrl_condition))
 }
